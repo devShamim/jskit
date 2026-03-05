f3087aaa6bccbf1f7fbc9263ed626a4a2e9fdcbc
Shamim Ahmed <shamimeforb@gmail.com>
2026-03-05 10:56:27 +0600

    updated number package

--- packages/fields/src/fields/number/index.tsx
+++ packages/fields/src/fields/number/index.tsx
@@ -11,42 +11,42 @@ import { getValue, isDisabled, updateAttribute } from '../../utils';
 import { NumberFieldProps } from './types';
 import { isFunction } from 'lodash';
 
-export default function Number( props: NumberFieldProps ): JSX.Element {
+export default function Number(props: NumberFieldProps): JSX.Element {
 	const { field, attributes, setAttributes } = props;
 
-	const handleChange = ( value: any ) => {
+	const handleChange = (value: any) => {
 		let processedValue;
-		if ( field.precision ) {
-			processedValue = value ? parseFloat( value ) : undefined;
-		}else{
-			const integerValue = parseInt( value, 10 );
-            processedValue = !isNaN( integerValue ) ? integerValue : undefined;
+		if (field.precision) {
+			processedValue = value ? parseFloat(value) : undefined;
+		} else {
+			const integerValue = parseInt(value, 10);
+			processedValue = !isNaN(integerValue) ? integerValue : undefined;
 		}
-		updateAttribute( processedValue, props );
+		updateAttribute(processedValue, props);
 
 		// Execute the custom onChange handler if defined
 		if (field.onChange && typeof field.onChange === 'function') {
-			field.onChange({value: processedValue, ...props});
+			field.onChange({ value: processedValue, ...props });
 		}
 	};
 
-	const min = isFunction( field?.min ) ? field.min( attributes ) : field?.min;
-	const max = isFunction( field?.max ) ? field.max( attributes ) : field?.max;
+	const min = isFunction(field?.min) ? field.min(attributes) : field?.min;
+	const max = isFunction(field?.max) ? field.max(attributes) : field?.max;
 
 	return (
 		<NumberComponent
 			//@ts-ignore
-			label={ <Label { ...props } /> }
-			description={ field.description }
+			label={<Label {...props} />}
+			description={field.description}
 			size="__unstable-large"
-			step={ 1 }
-			value={ getValue( props ) }
-			onChange={ handleChange }
-			disabled={ isDisabled( props ) }
-			min={ min }
-			max={ max }
-			required={ field?.required }
-			labelPosition={ field?.labelPosition }
+			step={field.step ?? 1}
+			value={getValue(props)}
+			onChange={handleChange}
+			disabled={isDisabled(props)}
+			min={min}
+			max={max}
+			required={field?.required}
+			labelPosition={field?.labelPosition}
 		/>
 	);
 }
--- packages/fields/src/fields/number/types.ts
+++ packages/fields/src/fields/number/types.ts
@@ -4,8 +4,9 @@ import { BaseField, BaseFieldProps } from '../../types/field';
 export type NumberFieldType = BaseField & {
 	type: 'number';
 	precision?: boolean;
-	min?: number | ( ( attributes: Record< string, any > ) => number );
-	max?: number | ( ( attributes: Record< string, any > ) => number );
+	step?: number;
+	min?: number | ((attributes: Record<string, any>) => number);
+	max?: number | ((attributes: Record<string, any>) => number);
 	labelPosition?: LabelPosition;
 };
 
